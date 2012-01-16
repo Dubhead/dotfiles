09f7a7cbdeed2a5bdd4dfba4aac44806aedeb62b
MIURA Masahiro <echochamber@gmail.com>
2012-01-16 15:44:03 +0900

    filecomp.py: handle subdirectories

--- filecomp.py
+++ filecomp.py
@@ -10,11 +10,16 @@ DOTFILES_DIR = os.path.join(HOME, 'dotfiles')
 EXCLUDES = [os.path.basename(sys.argv[0]), "README", ".git"]
 
 os.chdir(DOTFILES_DIR)
-for f in sorted(glob.glob('*') + glob.glob('.*')):
-    if f in EXCLUDES:
-        continue
-    # print(f)
-    dotfile_in_home = os.path.join(HOME, f)
-    subprocess.call(['diff', '-u', f, dotfile_in_home])
+for root, dirs, files in os.walk(DOTFILES_DIR):
+    for f in files:
+        if (root == DOTFILES_DIR) and f in EXCLUDES:
+            continue
+        dotfile_in_dotfiles = os.path.join(root, f)
+        dotfile_in_home = os.path.join(HOME,
+            dotfile_in_dotfiles[len(DOTFILES_DIR)+1:])
+        # print(dotfile_in_dotfiles, dotfile_in_home)
+        subprocess.call(['diff', '-u', dotfile_in_dotfiles, dotfile_in_home])
+        if '.git' in dirs:
+            dirs.remove('.git')
 
 # eof
