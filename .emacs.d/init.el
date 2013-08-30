139881c8806094fff9ace39f6dc9b58621e2a5f3
MIURA Masahiro <echochamber@gmail.com>
2013-08-30 13:00:34 +0900

    .emacs/init.el: Add setq package-archives.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,10 @@
 
 (add-to-list 'load-path "~/.emacs.d/")
 
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("melpa" . "http://melpa.milkbox.net/packages/")))
+
 (require 'wordcaps)
 
 (autoload 'dmacro-exec "dmacro" nil t)
